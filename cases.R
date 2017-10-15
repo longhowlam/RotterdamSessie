8b2aa6e67175c682ff34d56a9bf913725df42647
longhowlam <longhowlam@gmail.com>
2017-10-15 15:26:50 +0100

    add

---
+++ cases.R
@@ -0,0 +1,13 @@
+## machine learning slides ( nog met wat SAS voorbeelden :-)
+
+https://www.slideshare.net/LonghowLam/machine-learning-overview
+
+jaap huisprijzen
+
+ikea
+
+bold beautiful
+
+gaspedaal
+##
+
